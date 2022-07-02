f7001d991ad830585a7c0761b722e699db364cc2
Marlyse22 <mapagamarlyse@gmail.com>
2022-07-03 01:22:51 +0200

    front

---
+++ frontend/src/components/Head/HeadL.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeadL = () => {
+  return (
+    <div>
+        <Container>
+            <Row>
+                <Col>1 of 3</Col>
+                <Col xs={6}>2 of 3 (wider)</Col>
+                <Col>3 of 3</Col>
+            </Row>
+        </Container>
+    </div>
+  )
+}
+
+export default HeadL
